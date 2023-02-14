140e3083fff0b97a32408dfbab50dba7ac0d90ed
Vadim Cheremnykh <chere96@yandex.ru>
2023-02-14 21:31:55 +0400

    add Task9

---
+++ seminar2/task9/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int number = Random.Shared.Next(10, 100);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+
+int firstDigit = number / 10;
+if (firstDigit > lastDigit)
+{
+    // Вместо нескольких строк Console.WriteLine()
+    Console.WriteLine($"max {firstDigit}, min {lastDigit}");
+}
+else
+{
+    Console.WriteLine($"max {lastDigit}, min {firstDigit}");
+}
