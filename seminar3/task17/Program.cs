ca444c6b1fd902daa5456eca11992179e4a5a831
Vadim Cheremnykh <chere96@yandex.ru>
2023-02-28 12:00:08 +0400

    add seminar3, seminar4 and seminar5

---
+++ seminar3/task17/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int ReadNumber (string messageToUser)
+{
+     Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+
+int x = ReadNumber("Введите координаты Х");
+
+int y = ReadNumber("Введите координаты Y");
+
+if(x > 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в первой координатной четверти");
+}
+
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Точка находится во второй координатной четверти");
+}
+
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в третьей координатной четверти");
+}
+
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Точка находится в четвёртой координатной четверти");
+}
+
+else
+{
+    Console.WriteLine("Невозможно определить координатную четверть");
+}
