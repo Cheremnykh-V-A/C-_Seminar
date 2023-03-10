e399add635c09ecc360637544d0d10baea6e08cd
Vadim Cheremnykh <chere96@yandex.ru>
2023-03-10 21:33:11 +0400

    added FactorialRecursive

---
+++ seminar9/FactorialRecursive/Program.cs
@@ -0,0 +1,10 @@
+﻿uint number = 4;
+// uint - неотрицательное число
+uint Factorial (uint n)
+{
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+    uint result = n * Factorial(n - 1);
+    return result;
+}
+Console.WriteLine(Factorial(number));
