84dae2bc542a9a3067bb7ccf06646017db130794
Vadim Cheremnykh <chere96@yandex.ru>
2023-03-10 22:36:33 +0400

    added task67

---
+++ seminar9/task67/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int number = ReadNumber("Введите число");
+
+int PrintSumOfDigits (int number)
+{
+    int sum = 0;
+    sum += number % 10;
+    if(number > 10) 
+    {
+       sum += PrintSumOfDigits(number / 10);
+    }
+    return sum;
+}
+
+int result = PrintSumOfDigits(number);
+Console.WriteLine($"Сумма всех цифр этого числа = {result}");
